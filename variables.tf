30e07b2fda7d09229c4eb8fe12b9df99b428e92d
volodymyrhasych <vladimirgasich@gmail.com>
2025-11-25 18:53:56 +0100

    Add variables for ALB SG ID and public subnet IDs
    Added variables for ALB security group ID and public subnet IDs.

--- variables.tf
+++ variables.tf
@@ -7,3 +7,24 @@ variable "vpc_id" {
   type        = string
   description = "The VPC ID where ALB and ECS run"
 }
+variable "alb_sg_id" {
+  type = string
+}
+
+variable "public_subnet_ids" {
+  type = list(string)
+}
+# ===========================
+# ALB Listener (HTTP)
+# ===========================
+
+resource "aws_lb_listener" "fitaf_http_listener" {
+  load_balancer_arn = aws_lb.fitaf_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.fitaf_tg.arn
+  }
+}
