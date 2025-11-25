20bd2079a9aeb01eed31ce9ad60419aa6f32d233
volodymyrhasych <vladimirgasich@gmail.com>
2025-11-25 18:54:52 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -28,3 +28,10 @@ resource "aws_lb_listener" "fitaf_http_listener" {
     target_group_arn = aws_lb_target_group.fitaf_tg.arn
   }
 }
+variable "private_subnet_ids" {
+  type = list(string)
+}
+
+variable "ecs_sg_id" {
+  type = string
+}
